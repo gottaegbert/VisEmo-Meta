bbfd4818b899d8bbbc726698068a2ea358e12d05
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2022-07-08 17:11:07 +0200

    update emoji angry

--- Assets/HappyToggle.cs
+++ Assets/HappyToggle.cs
@@ -8,6 +8,12 @@ public class HappyToggle : MonoBehaviour
     double[] newResults;
     public Animator anim;
     public VisualEffect HappyParticle;
+    private VisualEffect Happyelement;
+
+    public VisualEffect AngryParticle;
+    private VisualEffect AngryElement;
+
+
     private bool excitingUp;
     float engagementData;
     int sequence;
@@ -16,72 +22,74 @@ public class HappyToggle : MonoBehaviour
     private float timer = 0;
     private float delayTime = 0.25f;
 
-    // Start is called before the first frame update
+    
+    void Start()
+    {
+        Happyelement = HappyParticle.GetComponent<VisualEffect>();
+    }
     void Update()
     {
-        if(anim != null && MainScript.results != null) 
+        newResults = MainScript.results;
+        engagementData = Function_DoubletoFloat.emoMath(newResults[9]);
+        (emotionData, sequence) = Function_BiggestEmojData.GetMax(newResults);
+        if (anim != null && MainScript.results != null) 
         {
-            newResults = MainScript.results;
-            (emotionData, sequence) = Function_BiggestEmojData.GetMax(newResults);
-            if (Input.GetKeyDown(KeyCode.H) && !excitingUp)
+            if (sequence == 3 && !excitingUp)
             {
                 
                 anim.SetTrigger("HappyUp");
-
-                if(HappyParticle != null)
+                
+                if (HappyParticle != null)
                 HappyParticle.Play();
                 Debug.Log("happytoggle");
 
                 excitingUp = true;
                 StartCoroutine(ResetBool(excitingUp,0.5f));
             }
+            if (sequence == 6)
+            {
+
+                //anim.SetTrigger("Angryup");
+
+                if (AngryParticle != null)
+                    AngryParticle.Play();
+                Debug.Log("angrytoggle");
+            }
 
             if (timer >= delayTime)
             {
-
-                //------------------------------------------Set color, size and environment color-----------------------------------------
-                selectEmotion();
                 selectSize();
-            
-                //Function_EnvironmentBar1.SetEnvironValue();
-
                 timer = 0;
-
             }
         }
     }
 
-    // Update is called once per frame
    IEnumerator ResetBool (bool boolToReset, float delay = 0.1f)
     {
         yield return new WaitForSeconds(delay);
         excitingUp = !excitingUp;
     }
 
-
-    void selectEmotion()
-    {
-
-        switch (sequence)
-        {
-            case 3:
-                Function_VFX_Meeting.Happiness(HappyParticle, emotionData);
-                break;
-
-            case 4:
-                Function_VFX_Meeting.Surprise(HappyParticle, emotionData);
-                break;
-
-
-            case 6:
-                Function_VFX_Meeting.Anger(HappyParticle, emotionData);
-                break;
-
-        }
-    }
     void selectSize()
     {
-        Function_VFX_Meeting.changeSize(HappyParticle, engagementData);
+        changeSize(HappyParticle, engagementData);
     }
+
+
+    public static void changeSize(VisualEffect visualEffect, float engagementData)
+    {
+        visualEffect.SetFloat("Size", engagementData / 100000);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
 
