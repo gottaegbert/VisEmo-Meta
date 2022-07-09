2c59d4b13602124060dbda76ae0ba4788e621d33
Truthlee0509 <liweiyukobe@gmail.com>
2022-07-09 21:57:09 +0200

    ImageElement

--- Assets/LWY/Scripts/ChangeEffect_Menu.cs
+++ Assets/LWY/Scripts/ChangeEffect_Menu.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 using System;
 using UnityEngine.VFX;
 
--- Assets/MaxemoProgress.cs
+++ Assets/MaxemoProgress.cs
@@ -2,23 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Michsky.UI.ModernUIPack;
-
+//using Michsky.UI.ModernUIPack;
+using UnityEngine.UI;
 
 public class MaxemoProgress : MonoBehaviour
 {
 
     double[] newResults;
     float emotionData;
+
     private Vector4 col;
+
+
     int sequence;
     private float timer = 0;
     private float delayTime = 1f;
-    public ProgressBar myBar;
+
+    //public ProgressBar myBar;
+
+    public Image image;
+    private float transparency=1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-         col = myBar.GetComponent<Color>();
+         //col = myBar.GetComponent<Color>();
     }
 
     // Update is called once per frame
@@ -27,26 +36,55 @@ public class MaxemoProgress : MonoBehaviour
         if (MainScript.results != null)
         {
             newResults = MainScript.results;
+
+            //Set bar color
+            //Set image element color
+
             (emotionData, sequence) = Function_BiggestEmojData.GetMax(newResults);
-            myBar.ChangeValue(emotionData);
+            selectEmotion();
+
+            image.color = col;
+            
+            
             //col = color[sequence];
+
+            //myBar.ChangeValue(emotionData);
+
+
+
+
         }
     }
 
     void selectEmotion()
     {
+        
 
         switch (sequence)
         {
             case 3:
-                col= new Vector4(0, emotionData / 100, emotionData / 200);
+                col= new Vector4(0, emotionData / 100, emotionData / 200, transparency);
                 break;
 
             case 4:
-                col = new Vector4(emotionData / 100, emotionData / 100, 0);
+                col = new Vector4(emotionData / 100, emotionData / 100, 0, transparency);
                 break;
 
-           
+            case 5:
+                col = new Vector4(0,emotionData / 200, emotionData / 100, transparency);
+                break;
+
+            case 6:
+                col = new Vector4(emotionData / 100, 0, 0, transparency);
+                break;
+
+            case 7:
+                col = new Vector4(emotionData / 200, emotionData / 200, 0, transparency);
+                break;
+
+            case 8:
+                col = new Vector4(emotionData / 100, emotionData / 200, 0, transparency);
+                break;
 
         }
     }
