63dadc4cee9b1d67fec43bf66bba5efe0c116555
Truthlee0509 <liweiyukobe@gmail.com>
2022-07-10 13:44:31 +0200

    BarColorChange

--- Assets/EngageProgress.cs
+++ Assets/EngageProgress.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Michsky.UI.ModernUIPack;
+using UnityEngine.UI;
 
 
 public class EngageProgress : MonoBehaviour
@@ -10,12 +11,17 @@ public class EngageProgress : MonoBehaviour
     double[] newResults;
     float engagementData;
     private float timer = 0;
-    private float delayTime = 1f;
+    private float delayTime = 0.1f;
+
+
     public ProgressBar myBar;
+    public GameObject LoadingBar;
+    private Image image;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = LoadingBar.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -25,8 +31,35 @@ public class EngageProgress : MonoBehaviour
         {
             newResults = MainScript.results;
             engagementData = Function_DoubletoFloat.emoMath(newResults[9])/100f;
-            //Debug.Log(engagementData);
-            myBar.ChangeValue(engagementData);
+
+
+
+            //----------------Set delay------------------
+            timer += Time.deltaTime;
+
+            if (timer >= delayTime)
+            {
+                myBar.ChangeValue(engagementData);
+                //Debug.Log(engagementData);
+
+
+                if (engagementData < 30.0f)
+                {
+                    image.color = new Color(0, 0.5f, 1);
+                }
+
+                else
+                {
+                    image.color = new Color(1, 0.7f, 0);
+                }
+
+                timer = 0;
+
+            }
+
+
+
+
         }
     }
 }
